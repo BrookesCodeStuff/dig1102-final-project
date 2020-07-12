b08afcc4f0b9bdbb56c07024bd246b838916d159
Brooke <brookepaglia@gmail.com>
2020-07-12 12:35:42 -0400

    Complete project tasks

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ This project builds upon Exercise 7, which was a simple to-do list app that acce
 
 ## Project Tasks
 
-- [ ] Display a form to the user to add todos.
-- [ ] When a user types a todo into the form and hits submit, the following should happen:
-  - [ ] A new todo should be displayed below any previous todos
-  - [ ] Every todo in the list should have a ✅ check button next to it to check off the todo and mark it complete
-  - [ ] Clicking the ✅ check should change the todo in someway, either marking it green or perhaps crossing it out
-  - [ ] Every todo should have an ❌ to the right that will delete the todo
-  - [ ] Clicking the ❌ should remove the todo from the page
+- [x] Display a form to the user to add todos.
+- [x] When a user types a todo into the form and hits submit, the following should happen:
+  - [x] A new todo should be displayed below any previous todos
+  - [x] Every todo in the list should have a ✅ check button next to it to check off the todo and mark it complete
+  - [x] Clicking the ✅ check should change the todo in someway, either marking it green or perhaps crossing it out
+  - [x] Every todo should have an ❌ to the right that will delete the todo
+  - [x] Clicking the ❌ should remove the todo from the page
--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-const todoField = document.querySelector('#todoField');
 const todoListDisplay = document.querySelector('#todoList');
 let todoList = [];
 
+// Submit button functionality
 document.querySelector('#todoForm').addEventListener('submit', function(event) {
   event.preventDefault();
-  todoList.push(todoField.value);
+  
+  // push todo item object to the array
+  const todoField = document.querySelector('#todoField');
+  todoList.push({text: todoField.value, complete: false});
 
-  let template = todoList.map(item => `
-  <li>${item}</li>
-  `)
+  // update the todo list with the new item
+  renderList();
 
-  todoListDisplay.innerHTML = template.join('');
-  todoField.value = '';
+  // clear the text input field and put the cursor back in (make focus)
+  event.target.reset();
   todoField.focus();
 });
+
+// adds the event listener to the ul element
+todoListDisplay.addEventListener('click', handleClick)
+
+// function to update the list of todo items
+function renderList() {
+    let template = todoList.map((item, index) => `
+  <li  data-key='${index}' class='todo-item ${item.complete ? 'completed' : ''}'>
+    <input id='btn-${index}' type='checkbox' title='Mark as complete' ${item.complete ? 'checked' : ''} class='complete'>
+    ${item.text}
+    <button data-key='${index}' class='delete' title='Delete todo item'>🗑️</button>
+  </li>
+  `)
+  
+  todoListDisplay.innerHTML = template.join('');
+}
+
+// determine if the complete or delete button was pressed and process item 
+// accordingly
+function handleClick(event) {
+  let el = event.srcElement
+  let key = el.parentElement.dataset.key;
+  let item = todoList[key];
+  if (el.classList.contains('complete')) {
+    handleComplete(el, key, item);
+  } else {
+    handleDelete(el, key, item);
+  }
+}
+
+// toggle completed status of the list item
+function handleComplete(el, key, item) {
+  item.complete = !item.complete;
+  el.parentElement.classList.toggle('completed');
+}
+
+// delete the item from the todo list and re-render the list
+function handleDelete(el, key, item) {
+  todoList.splice(key, 1);
+  renderList();
+}
